f01677db6a1386cb65421cf56c32bf9df59e67f5
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2023-01-04 21:56:53 -0800

    Day1 formatting

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,10 +1,10 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-/// Returns the total calories held by each elf.
-fn get_total_calories() -> Vec<i32> {
-    let mut total_calories: Vec<i32> = Vec::new(); // calories for each elf
-    let mut running_total = 0; // tracks calories as we add up
+/// Returns the calories held by each elf.
+fn get_each_calories() -> Vec<i32> {
+    let mut each_calories: Vec<i32> = Vec::new(); // calories for each elf
+    let mut running_sum = 0; // tracks calories as we add up
 
     // read lines from the file one-by-one
     let file = File::open("data/day1.txt").expect("Could not open file");
@@ -15,33 +15,30 @@ fn get_total_calories() -> Vec<i32> {
 
         if line.len() == 0 {
             // encountered newline
-            if running_total == 0 {
-                panic!("Did not expect zero running total");
-            }
-            total_calories.push(running_total);
-            running_total = 0;
+            each_calories.push(running_sum);
+            running_sum = 0;
         } else {
             // parse line
-            let calories = line.parse::<i32>().expect("Could not parse line");
-            running_total += calories;
+            let calories: i32 = line.parse().expect("Could not parse line");
+            running_sum += calories;
         }
     }
 
     // at end of file, append final running total
-    total_calories.push(running_total);
+    each_calories.push(running_sum);
 
-    return total_calories;
+    return each_calories;
 }
 
 fn main() {
-    let mut total_calories = get_total_calories();
+    let mut total_calories = get_each_calories();
 
     // sort the calories; the last element is the max
     total_calories.sort();
 
-    println!("Max calories: {}", total_calories.last().unwrap());
-    println!(
-        "Sum of top three calories: {}",
-        total_calories.iter().rev().take(3).sum::<i32>()
-    );
+    let max_calories = total_calories.last().unwrap();
+    println!("Max calories: {}", max_calories);
+
+    let top_three_sum: i32 = total_calories.iter().rev().take(3).sum();
+    println!("Sum of top three calories: {}", top_three_sum);
 }
