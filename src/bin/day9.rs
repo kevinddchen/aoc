ce4e36e6f9afb74935bd7e5f39e914f77bce0c86
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2023-01-28 22:53:11 -0800

    Add day9 part2

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -12,16 +12,17 @@ enum Motion {
 }
 
 struct Rope {
-    head: Vec2d,
-    tail: Vec2d,
+    length: usize,
+    knots: Vec<Vec2d>,
 }
 
 impl Rope {
-    fn new() -> Self {
-        Self {
-            head: Vec2d { x: 0, y: 0 },
-            tail: Vec2d { x: 0, y: 0 },
+    fn new(length: usize) -> Self {
+        let mut knots: Vec<Vec2d> = Vec::new();
+        for _ in 0..length {
+            knots.push(Vec2d { x: 0, y: 0 });
         }
+        Self { length, knots }
     }
 
     /// Returns true if the points are touching
@@ -29,8 +30,33 @@ impl Rope {
         (a.x - b.x).abs() <= 1 && (a.y - b.y).abs() <= 1
     }
 
+    /// Returns the new position of a tail knot given the new position of the head knot
+    fn move_towards(tail: &Vec2d, head: &Vec2d) -> Vec2d {
+        if Self::is_touching(tail, head) {
+            // tail and head are touching, so tail does not move
+            return *tail;
+        }
+        // move x of tail towards head
+        let mut x = tail.x;
+        if tail.x < head.x {
+            x += 1;
+        } else if tail.x > head.x {
+            x -= 1;
+        }
+        // move y of tail towards head
+        let mut y = tail.y;
+        if tail.y < head.y {
+            y += 1;
+        } else if tail.y > head.y {
+            y -= 1;
+        }
+        return Vec2d { x, y };
+    }
+
     /// Move the rope according to the given motion.
-    fn move_rope(&mut self, motion: &Motion){
+    fn move_rope(&mut self, motion: &Motion) {
+        let mut new_knots: Vec<Vec2d> = Vec::new();
+
         // find new head position
         let dir_vec = match motion {
             Motion::Up => Vec2d { x: 0, y: 1 },
@@ -38,13 +64,14 @@ impl Rope {
             Motion::Down => Vec2d { x: 0, y: -1 },
             Motion::Left => Vec2d { x: -1, y: 0 },
         };
-        let new_head = self.head + dir_vec;
+        new_knots.push(self.knots[0] + dir_vec);
 
-        if !Rope::is_touching(&new_head, &self.tail) {
-            // New head is not touching, so tail moves to old head position
-            self.tail = self.head;
+        // move all other knots
+        for i in 1..self.length {
+            new_knots.push(Self::move_towards(&self.knots[i], &new_knots[i - 1]));
         }
-        self.head = new_head;
+
+        self.knots = new_knots;
     }
 
     /// Returns list of tail positions after moving the rope according to the
@@ -54,7 +81,8 @@ impl Rope {
 
         for motion in motions {
             self.move_rope(motion);
-            tail_positions.push(self.tail);
+            let tail = self.knots[self.length - 1];
+            tail_positions.push(tail);
         }
 
         return tail_positions;
@@ -105,9 +133,13 @@ fn count_unique_tail_positions(rope: &mut Rope, motions: &Vec<Motion>) -> i32 {
 }
 
 fn main() {
-    let mut rope = Rope::new();
     let motions = get_motions();
 
+    let mut rope = Rope::new(2);
     let count = count_unique_tail_positions(&mut rope, &motions);
     println!("Part 1: {}", count);
+
+    let mut rope = Rope::new(10);
+    let count = count_unique_tail_positions(&mut rope, &motions);
+    println!("Part 2: {}", count);
 }
