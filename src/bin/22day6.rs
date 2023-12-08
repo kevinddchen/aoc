ccd47bf7320ebe5199bc401ff4ff01c0f3f8f4ca
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2023-12-07 22:38:41 -0800

    refactor 22day6

--- src/bin/22day6.rs
+++ src/bin/22day6.rs
@@ -1,5 +1,7 @@
 use std::collections::HashSet;
-use std::fs;
+
+// File containing puzzle data
+const DATA: &str = "data/22day6.txt";
 
 /// Given a string slice, return true if all characters are unique
 fn all_unique_chars(s: &str) -> bool {
@@ -12,8 +14,9 @@ fn all_unique_chars(s: &str) -> bool {
     return true;
 }
 
-/// Given a string slice, returns index at which the first contiguous run of `size` unique characters is found.
-/// I.e. if the output is `i`, then `s[i..i+size]` will be a contiguous run of unique characters.
+/// Given a string slice, returns index at which the first contiguous run of
+/// `size` unique characters is found. i.e. if the output is `i`, then
+/// `s[i..i+size]` will be a contiguous run of unique characters.
 fn find_unique_run(size: usize, s: &str) -> usize {
     for i in 0..s.len() - size {
         let slice = &s[i..i + size];
@@ -28,13 +31,13 @@ fn main() {
     const SOP_MARKER_SIZE: usize = 4; // start-of-packet marker size
     const SOM_MARKER_SIZE: usize = 14; // start-of-message marker size
 
-    let datastream = fs::read_to_string("data/day6.txt").expect("Could not read file");
+    let data = std::fs::read_to_string(DATA).unwrap();
 
-    let sop_marker_index = find_unique_run(SOP_MARKER_SIZE, &datastream);
+    let sop_marker_index = find_unique_run(SOP_MARKER_SIZE, &data);
     let packet_start = sop_marker_index + SOP_MARKER_SIZE;
-    println!("Part 1: {packet_start}");
+    println!("Part 1: {packet_start}"); // Expected output: 1361
 
-    let som_marker_index = find_unique_run(SOM_MARKER_SIZE, &datastream[packet_start..]);
+    let som_marker_index = find_unique_run(SOM_MARKER_SIZE, &data[packet_start..]);
     let message_start = som_marker_index + SOM_MARKER_SIZE + packet_start;
-    println!("Part 2: {message_start}");
+    println!("Part 2: {message_start}"); // Expected output: 3263
 }
